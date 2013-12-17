4fcf0d6d39b22a787adafdecf0f139dbc46786a0
WillianMGoncalves <willian@exla.com.br>
2013-12-17 13:26:51 -0200

    Circleci / Grunt

--- testes/crud.js
+++ testes/crud.js
@@ -3,16 +3,46 @@ var crud = require("../servidor/modelos/crud");
 var assert = require("assert");
 
 describe('Crud', function(){
-    describe('obterTudo', function(){
-        it('Obter tudo permite parametros errados', function(){
-            var funcaoFalha = function( mensagemErro ){
-                throw 'Errofa';
-            };
-            var funcaoSucesso = function( resposta ){
-                throw 'Erro';
-            }
+    var funcaoFalhaCorreta = function( mensagemErro ){
+        assert.ok('Mensagem de erro exibida adequadamente: ' + mensagemErro );
+    };
 
-            crud.obterTudo( null,funcaoSucesso,funcaoFalha );
-        })
-    })
-})
+    var funcaoSucessoIncorreta = function( resposta ){
+        throw 'Erro';
+    };
+
+    describe('obterTudo', function(){
+        it('Obter tudo permite endereços errados', function(){
+            crud.obterTudo( null,funcaoSucessoIncorreta,funcaoFalhaCorreta );
+        });
+        it('Obter tudo permite funcaoSucesso inválida', function(){
+            assert.throws( function(){
+                crud.obterTudo( 'administradores',null,funcaoFalhaCorreta);
+            });
+        });
+        it('Obter tudo permite funcaoFalha inválida', function(){
+            assert.throws( function(){
+                crud.obterTudo( 'administradores',funcaoSucessoIncorreta,null);
+            });
+        });
+    });
+
+    describe('inserir', function(){
+        var novosDados = {
+            campo1: 'valor1',
+            campo2: 'valor2',
+            campo3: 'valor3'
+        };
+
+        var funcaoFalha = function(){
+            throw 'Erro';
+        };
+        var funcaoSucesso = function( dados ){
+            assert.notEqual( dados.indexOf(dados), -1 );
+        }
+        it('Inserir adiciona parametros elementos', function(){
+
+            crud.inserir('teste', novosDados, funcaoSucesso, funcaoFalha );
+        });
+    });
+});
