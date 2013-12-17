871af4a0f8be3f847123036819d8740641eb76b5
WillianMGoncalves <willian@exla.com.br>
2013-12-17 13:06:33 -0200

    Circleci / Grunt

--- testes/crud.js
+++ testes/crud.js
@@ -6,7 +6,7 @@ describe('Crud', function(){
     describe('obterTudo', function(){
         it('Obter tudo permite parametros errados', function(){
             var funcaoFalha = function( mensagemErro ){
-
+                throw 'Errofa';
             };
             var funcaoSucesso = function( resposta ){
                 throw 'Erro';
