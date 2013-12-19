a347692941951101efe9d3f93f20213a28c02436
WillianMGoncalves <willian@exla.com.br>
2013-12-19 08:48:37 -0200

    Webdriver (seleninum) teste interface de login

--- testesInterface/login.js
+++ testesInterface/login.js
@@ -1,27 +1,26 @@
 /*global describe, require, it, before*/
 
 var assert = require('assert'),
-    webdriver = require('selenium-webdriver'),
-    driver;
+    webdriver = require('selenium-webdriver'),driver;
 
 // tests
 describe('Login', function() {
     it('Campos', function(done) {
 
-        driver = new webdriver
-            .Builder()
-            .usingServer('http://urna.herokuapp.com')
-            .withCapabilities(
-            {
-                'browserName': 'chrome'
-            })
-            .build();
+        var webdriver = require('selenium-webdriver');
+
+        var driver = new webdriver.Builder().
+            withCapabilities(webdriver.Capabilities.chrome()).
+            build();
+
+        driver.get('http://urna.herokuapp.com/');
+
         var searchBox = driver.findElement(webdriver.By.id('formulario-autenticacao-login'));
         searchBox.sendKeys('webdriver\n').then(function() {
             return searchBox.getAttribute('value');
-        }).then(function(value) {
-                assert.equal(value, 'webxdriver');
-                done();
-            });
+        }).then(function (value) {
+                    assert.equal(value, 'webxdriver');
+                    done();
+                });
     });
 });
